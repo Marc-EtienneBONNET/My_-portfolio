682d922e8f79055de1d8d0f4fb37bc3b61f61274
Marc-Etienne Bonnet <webbonnet@gmail.com>
2022-10-08 09:28:56 +0200

    syncro

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ComposantAddNewUser from './lib_react/1_composantAddNewUser/app/App'
+import ComposantAddNewUser from './lib_react/1_composantAddNewUser/composantGlobal/addNewUser'
+import ComposantConnectUser from './lib_react/2_composantConnectUser/composantGlobal/composantConnectUser'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <div style={{height:'100%', width:'100%'}}>
+      <ComposantConnectUser/>
+    </div>
+    <div style={{height:'100%', width:'100%'}}>
       <ComposantAddNewUser/>
+    </div>
   </React.StrictMode>
 );
